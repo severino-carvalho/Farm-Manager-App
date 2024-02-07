6cd30b7e9f11703a5ea4d79e0f8a56fe4cdf7cdf
Severino Carvalho <severinocarvalho14@gmail.com>
2024-02-07 01:34:33 -0300

    feat: add nestjs to fma-manager
    We decided to create a back-end using nestjs to study the technology in more depth.
    
    This commit will add everything we need to use nestjs.

---
+++ fma-manager/src/main.ts
@@ -0,0 +1,12 @@
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  await app.listen(3003);
+
+  Logger.verbose('Server running on port 3003', 'INICIALIZATION');
+}
+bootstrap();
